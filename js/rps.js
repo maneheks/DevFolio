b32eca8389d7d36591f84699aa5ee3c27a87ca90
Lydia <lydiakristinee@outlook.com>
2023-09-15 01:15:19 +0930

    Initial loading of rock paper scissors files

---
+++ js/rps.js
@@ -0,0 +1,15 @@
+function rock() {
+    rpsPlayerImg.src = "../images/rps-rock.png"
+}
+
+function paper() {
+    rpsPlayerImg.src = "../images/rps-paper.png"
+}
+
+function scissors() {
+    rpsPlayerImg.src = "../images/rps-scissors.png"
+}
+
+function reset() {
+    rpsPlayerImg.src = "../images/rps-default-img.png"
+}
