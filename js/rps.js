a9a691a31572025c0968d1611080593743594916
maneheks <lydiakristinee@outlook.com>
2023-09-19 22:00:19 +0930

    Computer now randomly selects rock, paper or scissors when the user selects

--- js/rps.js
+++ js/rps.js
@@ -1,15 +1,54 @@
+var compChoiceIndex;
+
 function rock() {
-    rpsPlayerImg.src = "../images/rps-rock.png"
+    rpsPlayerImg.src = "../images/rps-rock.png";
+    compChoiceIndex = Math.floor(Math.random()*3) + 1;
+    switch (compChoiceIndex) {
+        case 1: 
+            rpsComputerImg.src = "../images/rps-paper.png";
+            break;
+        case 2: 
+            rpsComputerImg.src = "../images/rps-scissors.png";
+            break;
+        case 3: 
+            rpsComputerImg.src = "../images/rps-rock.png";
+            break;
+    }
 }
 
 function paper() {
-    rpsPlayerImg.src = "../images/rps-paper.png"
+    rpsPlayerImg.src = "../images/rps-paper.png";
+    compChoiceIndex = Math.floor(Math.random()*3) + 1;
+    switch (compChoiceIndex) {
+        case 1: 
+            rpsComputerImg.src = "../images/rps-paper.png";
+            break;
+        case 2: 
+            rpsComputerImg.src = "../images/rps-scissors.png";
+            break;
+        case 3: 
+            rpsComputerImg.src = "../images/rps-rock.png";
+            break;
+    }
 }
 
 function scissors() {
-    rpsPlayerImg.src = "../images/rps-scissors.png"
+    rpsPlayerImg.src = "../images/rps-scissors.png";
+    compChoiceIndex = Math.floor(Math.random()*3) + 1;
+    switch (compChoiceIndex) {
+        case 1: 
+            rpsComputerImg.src = "../images/rps-paper.png";
+            break;
+        case 2: 
+            rpsComputerImg.src = "../images/rps-scissors.png";
+            break;
+        case 3: 
+            rpsComputerImg.src = "../images/rps-rock.png";
+            break;
+    }
 }
 
 function reset() {
-    rpsPlayerImg.src = "../images/rps-default-img.png"
+    rpsPlayerImg.src = "../images/rps-default-img.png";
+    rpsComputerImg.src = "../images/rps-default-img.png";
 }
